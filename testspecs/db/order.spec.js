5527501e17b4452c97c5be8853b8832bc8c003b4
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-03-02 16:16:11 -0500

    fix(test/order):updating tests

--- testspecs/db/order.spec.js
+++ testspecs/db/order.spec.js
@@ -5,40 +5,37 @@ const db = require('../../server/db')
 const Order = db.model('order')
 
 describe('Order model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
+  beforeEach(async () => {
+    await db.sync({force: true})
   })
-
-  describe('order quantity instance methods', () => {
+  describe('ensure order quantity has a check for null', () => {
     it('order quantity cannot be null', async () => {
       await expect(
         Order.create({
           quantity: null,
           shippingCost: 1,
           currentOrder: false,
-          shippingAddress: '115 ave A New York, New York 10016',
-          productId: 1,
-          userId: 1
+          shippingAddress: '115 ave A New York, New York 10016'
         }),
-        "We shouldn't be able to create a order with no quantity"
-      ).to.be.rejected
+        "We shouldn't be able to create an order with no quantity"
+      ).should.be.rejected
     })
   })
-  describe('shipping cost instance methods', () => {
+
+  describe('ensure shipping cost has a check for null', () => {
     it('shipping cost cannot be null', async () => {
       await expect(
         Order.create({
           quantity: 5,
           shippingCost: null,
           currentOrder: false,
-          shippingAddress: '115 ave A New York, New York 10016',
-          productId: 1,
-          userId: 1
+          shippingAddress: '115 ave A New York, New York 10016'
         }),
         "We shouldn't be able to create a order without shipping cost"
-      ).to.be.rejected
+      ).should.be.rejected
     })
   })
+
   describe('address instance methods', () => {
     it('address cannot be an empty string', async () => {
       await expect(
@@ -46,12 +43,10 @@ describe('Order model', () => {
           quantity: 5,
           shippingCost: 1,
           currentOrder: false,
-          shippingAddress: '',
-          productId: 1,
-          userId: 1
+          shippingAddress: ''
         }),
         "We shouldn't be able to create a order with an empty address"
-      ).to.be.rejected
+      ).should.be.rejected
     })
   })
 })
