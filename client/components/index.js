20adbde90f70a4fbd4fef51a42e54ab25425db2c
Yizhou Shui <yvonne.shui126@gmail.com>
2021-03-03 16:29:09 -0500

    FE:HomepageComponents

--- client/components/index.js
+++ client/components/index.js
@@ -5,4 +5,6 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as HomePage} from './HomePage'
+
 export {Login, Signup} from './auth-form'
