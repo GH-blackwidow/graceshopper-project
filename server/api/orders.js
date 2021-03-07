aa46ab0d2b3d5411b63945c6e81db323ded5fe66
Kelsey Greene <kelsey.greene17@gmail.com>
2021-03-07 18:01:36 -0500

    Added correctUser verification to cart API and admin verification to Order API

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,21 +1,22 @@
 const router = require('express').Router()
 const {Order, Product} = require('../db/models')
+const {verifyAdmin} = require('../utils/gatekeeping')
 module.exports = router
 
 //get all past user orders
-router.get('/:userId', async (req, res, next) => {
+router.get('/:userId', verifyAdmin, async (req, res, next) => {
   try {
     //there's no virtual field for subtotal
     const pastOrders = await Order.findAll({
       where: {
         isCurrent: false,
-        userId: req.params.userId,
+        userId: req.params.userId
       },
       include: [
         {
-          model: Product,
-        },
-      ],
+          model: Product
+        }
+      ]
     })
     res.json(pastOrders)
   } catch (error) {
