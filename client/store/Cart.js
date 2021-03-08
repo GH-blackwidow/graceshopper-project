a05b2087ac2a590c623223e060356e6ab5a6869a
Elizabeth Woodhouse <elizabethjpwoodhouse@gmail.com>
2021-03-08 16:32:28 -0500

    fix: cart added add and delete reducers

--- client/store/Cart.js
+++ client/store/Cart.js
@@ -3,20 +3,30 @@ import axios from 'axios'
 //ACTION TYPE
 const GET_CART = 'GET_CART'
 const UPDATE_CART = 'UPDATE_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
+const DELETE_FROM_CART = 'DELETE_FROM_CART'
 
 //ACTION CREATORS
-const getCart = (cart) => ({
+const getCart = cart => ({
   type: GET_CART,
-  cart,
+  cart
 })
-const updateCart = (newCart) => ({
+const updateCart = newCart => ({
   type: UPDATE_CART,
-  newCart,
+  newCart
+})
+const addToCart = item => ({
+  type: ADD_TO_CART,
+  item
+})
+const deleteFromCart = deletedItem => ({
+  type: DELETE_FROM_CART,
+  deletedItem
 })
 
 //THUNK CREATORS
 //pass null to userId if someone is not signed in
-export const fetchCart = (userId) => async (dispatch) => {
+export const fetchCart = userId => async dispatch => {
   try {
     const cart = await axios.get(`/api/cart/${userId}`)
     dispatch(getCart(cart))
@@ -24,7 +34,7 @@ export const fetchCart = (userId) => async (dispatch) => {
     console.log('Can not retrieve cart', err)
   }
 }
-export const editCart = (userId, newCart) => async (dispatch) => {
+export const editCart = (userId, newCart) => async dispatch => {
   try {
     await axios.put(`/api/cart/${userId}`, newCart)
     dispatch(updateCart(newCart))
@@ -32,15 +42,49 @@ export const editCart = (userId, newCart) => async (dispatch) => {
     console.log('Can not update cart', err)
   }
 }
+export const addToCartThunk = (orderId, item) => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.put(`/api/cart/${orderId}`, item)
+      if (data) {
+        dispatch(addToCart(data))
+      }
+    } catch (err) {
+      console.log('add to cart thunk error: ', err)
+    }
+  }
+}
+export const deleteFromCartThunk = (orderId, item) => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.delete(`/api/cart/${orderId}`, item)
+      if (data) {
+        dispatch(deleteFromCart(data))
+      }
+    } catch (err) {
+      console.log('add to cart thunk error: ', err)
+    }
+  }
+}
 
 //INITIAL STATE
 const defaultCart = {}
 
 //REDUCER
-export default function (state = defaultCart, action) {
+export default function(state = defaultCart, action) {
   switch (action.type) {
     case GET_CART:
       return action.cart
+    case ADD_TO_CART:
+      return [...state, action.item]
+    case DELETE_FROM_CART:
+      let newCart = []
+      action.cart.forEach(item => {
+        if (item !== action.deletedItem) {
+          newCart.push(item)
+        }
+      })
+      return newCart
     case UPDATE_CART:
       return action.newCart
     default:
