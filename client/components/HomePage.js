22c234f2c58fa6384f95b50e8c74da869e9e1655
Jackie Maratea <jacqueline.maratea@gmail.com>
2021-03-08 20:53:05 -0500

    testing logo

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,17 +1,12 @@
 import React from 'react'
-// import beershopper from './public/beershopper.png'
 
 function HomePage() {
   return (
-    <div>
+    <div className="logo" >
       <h1>Welcome to our Grace Hopper homepage! </h1>
-      <h3>The hoppiest place on the web! </h3>
-      <img
-        // src={beershopper}
-        alt="beermagic"
-        style={{width: '500px'}}
-      />
+      <h3>The hoppiest place on the web! </h3> 
     </div>
   )
 }
 export default HomePage
+
